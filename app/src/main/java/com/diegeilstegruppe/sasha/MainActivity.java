47424324d2150ac316841943b03fcd294a905a09
Denys Kibish <dkibish@1drop.de>
2017-05-20 01:20:16 +0200

    [TASK] added SpeechRecorder

--- app/src/main/java/com/diegeilstegruppe/sasha/MainActivity.java
+++ app/src/main/java/com/diegeilstegruppe/sasha/MainActivity.java
@@ -1,27 +1,47 @@
 package com.diegeilstegruppe.sasha;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import com.diegeilstegruppe.sasha.audio.SpeechRecorder;
 import com.diegeilstegruppe.sasha.network.Communicator;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    // TODO: only test
+    private static final int SPEECH_REQUEST_CODE = 0;
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
     private Communicator communicator;
+    private SpeechRecorder speechRecorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
         communicator = new Communicator();
+        speechRecorder = new SpeechRecorder(this);
 
         Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+
         button.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -32,5 +52,74 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                displaySpeechRecognizer();
+//            }
+//        });
+
+        button2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.d(TAG, "pressed");
+                        speechRecorder.startRecording();
+                        return true; // if you want to handle the touch event
+                    case MotionEvent.ACTION_UP:
+                        Log.d(TAG, "released");
+                        speechRecorder.stopAndPlayRecording();
+                        return true; // if you want to handle the touch event
+                }
+                return false;
+            }
+        });
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (speechRecorder != null) {
+            speechRecorder.onActivityStop();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+    }
+
+//    // Create an intent that can start the Speech Recognizer activity
+//    private void displaySpeechRecognizer() {
+//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        // Start the activity, the intent will be populated with the speech text
+//        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+//    }
+//
+//    // This callback is invoked when the Speech Recognizer returns.
+//    // This is where you process the intent and extract the speech text from the intent.
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode,
+//                                    Intent data) {
+//        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+//            List<String> results = data.getStringArrayListExtra(
+//                    RecognizerIntent.EXTRA_RESULTS);
+//            String spokenText = results.get(0);
+//            // Do something with spokenText
+//
+//            Log.d(TAG, spokenText);
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 }
--- app/src/main/java/com/diegeilstegruppe/sasha/network/BasicAuthInterceptor.java
+++ app/src/main/java/com/diegeilstegruppe/sasha/network/BasicAuthInterceptor.java
@@ -2,7 +2,6 @@ package com.diegeilstegruppe.sasha.network;
 
 import java.io.IOException;
 
-import okhttp3.Credentials;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -16,16 +15,14 @@ public class BasicAuthInterceptor implements Interceptor {
     private String credentials;
 
     public BasicAuthInterceptor(String user, String password) {
-        this.credentials = Credentials.basic(user, password);
+        this.credentials = user + " " + password;
     }
 
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request request = chain.request();
         Request authenticatedRequest = request.newBuilder()
-                // .header("Authorization", credentials).build();
-                // TODO: why so sick?
-                .header("Authorization", "Bearer 66NIUDMXAU6R25XPLDJXKL72S3BVVVWN").build();
+                .header("Authorization", this.credentials).build();
 
         return chain.proceed(authenticatedRequest);
     }
