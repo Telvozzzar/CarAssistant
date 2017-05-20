dc162d3458666cd4bd8349106c8ccce4ac01dfbb
Alina <alina.edelmann@st.oth-regensburg.de>
2017-05-20 02:03:48 +0200

    First TTS try

--- app/src/main/java/com/diegeilstegruppe/sasha/MainActivity.java
+++ app/src/main/java/com/diegeilstegruppe/sasha/MainActivity.java
@@ -1,7 +1,10 @@
 package com.diegeilstegruppe.sasha;
 
-import android.os.Bundle;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -13,6 +16,8 @@ import android.util.Log;
 
 import com.diegeilstegruppe.sasha.audio.SpeechRecorder;
 import com.diegeilstegruppe.sasha.network.Communicator;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,12 +34,23 @@ public class MainActivity extends AppCompatActivity {
     private Communicator communicator;
     private SpeechRecorder speechRecorder;
 
+    TextToSpeech t1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView text = (TextView) findViewById(R.id.textView);
+
+        t1 = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.GERMAN);
+                }
+            }
+        });
 
         final Switch switch_activated = (Switch) findViewById(R.id.switch_activated);
         switch_activated.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -100,4 +116,21 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //        super.onActivityResult(requestCode, resultCode, data);
 //    }
+
+    protected void tts(String text){
+        if (Build.VERSION.SDK_INT >= 21){
+            t1.speak((CharSequence) text, MODE_PRIVATE, null, null);
+
+        } else {
+            t1.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    public void onPause(){
+        if (t1 != null) {
+            t1.stop();
+            t1.shutdown();
+        }
+        super.onPause();
+    }
 }
