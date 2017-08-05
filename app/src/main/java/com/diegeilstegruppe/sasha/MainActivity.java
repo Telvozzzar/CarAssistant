4a0898a8c61a19046cab9079ed58da752502bcec
Telvozzzar <Telvozzzar@googlemail.com>
2017-08-06 01:28:08 +0200

    Spotify can now search and play songs

--- app/src/main/java/com/diegeilstegruppe/sasha/MainActivity.java
+++ app/src/main/java/com/diegeilstegruppe/sasha/MainActivity.java
@@ -1,30 +1,68 @@
 package com.diegeilstegruppe.sasha;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.diegeilstegruppe.sasha.audio.Speech;
 import com.diegeilstegruppe.sasha.audio.SpeechRecorder;
 import com.diegeilstegruppe.sasha.audio.WavAudioRecorder;
 import com.diegeilstegruppe.sasha.network.Communicator;
 import com.diegeilstegruppe.sasha.service.Notifications.BusProvider;
-import com.diegeilstegruppe.sasha.service.Spotify.SpotifyPlayerReference;
-import com.diegeilstegruppe.sasha.service.Spotify.spotifyController;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
 import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.TracksPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainActivity extends AppCompatActivity implements SpotifyPlayer.NotificationCallback,ConnectionStateCallback {
 
     private static final String TAG = "MainActivity";
 
+
+    //Spotify Player
+    private static final String CLIENT_ID = "7ae9d4102d804979b912d01b36b4fe66";
+    private static final String REDIRECT_URI = "yourcustomprotocol://callback";
+    private static String ACCESS_TOKEN;
+    // Request code that will be used to verify if the result comes from correct activity
+    // Can be any integer
+    private static final int REQUEST_CODE = 1337;
+    private Player mPlayer;
+    private SpotifyApi api = new SpotifyApi();
+
+
     // TODO: only test
     private static final int SPEECH_REQUEST_CODE = 0;
-    private static final int REQUEST_CODE = 1337;
-
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
     private boolean permissionToRecordAccepted = false;
@@ -92,12 +130,42 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate");
 
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
-
-        speechRecorder = new SpeechRecorder(this);
-        speech = new Speech(this);
 */
-            Intent intent = new Intent(this, spotifyController.class);
-            startActivityForResult(intent, REQUEST_CODE);
+
+        //Spotify Player
+            AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                    AuthenticationResponse.Type.TOKEN,
+                    REDIRECT_URI);
+            builder.setScopes(new String[]{"user-read-private", "streaming"});
+            AuthenticationRequest request = builder.build();
+
+            AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+            Button fab = (Button) findViewById(R.id.btn_record);
+            final SpotifyService spotify = api.getService();
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String searchquery = ((EditText)findViewById(R.id.tb_search_query)).getText().toString();
+                    //the following code snipped uses the WebApi to find tracks you can also search Albums or anything.
+                    spotify.searchTracks(searchquery, new Callback<TracksPager>() {
+                        @Override
+                        public void success(TracksPager tracksPager, Response response) {
+                            String s = tracksPager.tracks.items.iterator().next().uri;   //get first element of results
+                            mPlayer.playUri(null,s,0,0); //this is the SpotifyPlayer. Just check its methods
+                        }
+
+                        @Override
+                        public void failure(RetrofitError error) {
+                            Log.d("Callback Failure", error.getMessage());
+
+                        }
+                    });
+
+                }
+            });
+
         }
         catch (Exception e){
             e.printStackTrace();
@@ -105,15 +173,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case REQUEST_CODE:
-                SpotifyPlayerReference.player.playUri(null, "spotify:track:3d9DChrdc6BOeFsbrZ3Is0", 0, 0);
-                HttpRequestTask.doSomething(player,SpotifyPlayerReference.accessToken);
-        }
-    }
+
 
     @Override
     protected void onStop() {
@@ -148,5 +208,89 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         BusProvider.getInstance().register(this);
 
+    }
+    @Override
+    protected void onDestroy() {
+        // VERY IMPORTANT! This must always be called or else you will leak resources
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            case kSpPlaybackNotifyAudioDeliveryDone:
+                Log.d("onPlayBackEvent","kSpPlayBackNotifyAudioDelifery");
+                mPlayer.skipToNext(null);
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+        api.setAccessToken(ACCESS_TOKEN);
+        mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            final AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
+                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                    @Override
+                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
+                        mPlayer = spotifyPlayer;
+                        mPlayer.addConnectionStateCallback(MainActivity.this);
+                        mPlayer.addNotificationCallback(MainActivity.this);
+                        ACCESS_TOKEN = response.getAccessToken();
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
+                    }
+                });
+            }
+        }
     }
 }
